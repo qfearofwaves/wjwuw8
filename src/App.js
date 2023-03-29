8758acede38a765102cd70a0e37702cc2dfb06ce
qfearofwaves <qingniu.me@gmail.com>
2023-03-29 16:23:30 -0700

    style: add comments in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// this creates a component
+// in React, a component is a piece of code that represents 
+// a resuable part of a user interface
+// `export` makes this function available outside of this file
+// The `default` keyword tells other files using your code that it’s the main function in your file.
 export default function Square() {
+  // <button> is a JSX element
   return <button className="square">X</button>;
 }
